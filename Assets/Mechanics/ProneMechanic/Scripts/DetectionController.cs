6cb8f2e25cf1c041fbdad8b58c849c5c67b795a5
Cornapple <1031995@student.sae.edu.au>
2024-11-30 17:40:57 +1100

    more work on detection mode added aiming UI and debug UI

--- Assets/Mechanics/ProneMechanic/Scripts/DetectionController.cs
+++ Assets/Mechanics/ProneMechanic/Scripts/DetectionController.cs
@@ -6,6 +6,7 @@ public class DetectionController : MonoBehaviour
 {
     protected RaycastHit seen;
     public GameObject player;
+    public Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,14 @@ public class DetectionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        seen = GetComponent<RaycastHit>();
 
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(100);
     }
 }
